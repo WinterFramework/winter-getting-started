2613c2320570ce0afd0f80f89ed7e478f11a02f9
mofr <mofr@zond.org>
2022-08-04 19:25:08 +0200

    Try moving winter setup to settings

--- simple_api/settings.py
+++ simple_api/settings.py
@@ -129,3 +129,14 @@ REST_FRAMEWORK = {
 SWAGGER_SETTINGS = {
     'DEFAULT_AUTO_SCHEMA_CLASS': 'winter_openapi.SwaggerAutoSchema',
 }
+
+import winter.core
+import winter_django
+import winter_openapi
+from injector import Injector
+
+
+winter.core.set_injector(Injector())
+winter.web.setup()
+winter_django.setup()
+winter_openapi.setup(allow_missing_raises_annotation=True)
