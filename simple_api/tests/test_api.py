d593de2099c6061a02cbf99971a330c0727410f6
mofr <mofr@zond.org>
2022-08-12 18:14:11 +0200

    Intentionally break tests

--- simple_api/tests/test_api.py
+++ simple_api/tests/test_api.py
@@ -3,7 +3,7 @@ import requests
 
 def test_simple_api(live_server_url: str):
     client = requests.Session()
-    http_response = client.get(f'{live_server_url}/greeting/')
+    http_response = client.get(f'{live_server_url}/greeting/1')
 
     assert http_response.status_code == 200
     assert http_response.json() == 'Hello from Winter API!'
