585567689f1a375ca500927910aff2d1f208dd09
mofr <mofr@zond.org>
2022-08-13 12:58:20 +0200

    Remove unnecessary setup_django fixture as it's done by wsgi.py

--- conftest.py
+++ conftest.py
@@ -1,17 +1,9 @@
-import django
 import pytest
-import os
 import httpx
 from simple_api.wsgi import application
 
 
-@pytest.fixture(scope='session')
-def setup_django():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'simple_api.settings')
-    django.setup()
-
-
 @pytest.fixture()
-def api_client(setup_django):
+def api_client():
     with httpx.Client(app=application, base_url='http://127.0.0.1') as client:
         yield client
